540139a75e1a42be3a6e38a225827ba47b6b8db6
oykueata <oeatalay72@gmail.com>
2022-03-17 20:08:25 +0100

    Git1 and Git3 files updated

--- src/test/java/day1/Git1.java
+++ src/test/java/day1/Git1.java
@@ -2,4 +2,5 @@ package day1;
 
 public class Git1 {
     //git1 eklendi ve degistirildi(modified)
+    //commit sonrasi git 1 degisti
 }
--- src/test/java/day1/Git3.java
+++ src/test/java/day1/Git3.java
@@ -2,4 +2,5 @@ package day1;
 
 public class Git3 {
     //git3 olusturuldu
+    ////commit sonrasi git 3 degisti
 }
